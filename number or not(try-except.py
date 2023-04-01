81c966435de3f71eb72fcd5ebbd64000d88d3807
Sparsh Gupta <66106648+SparshOM@users.noreply.github.com>
2023-04-01 13:34:30 +0530

    Create number or not(try-except.py

---
+++ number or not(try-except.py
@@ -0,0 +1,11 @@
+numm =input("Type Number-> ")
+try:
+    num= int(numm)
+except:
+    num = -1
+if num > 0:
+    print("A Number")
+elif num ==0:
+    print("Zero")
+else:
+    print("Not A Number")
