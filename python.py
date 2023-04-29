34dfb0a982b15700c4176fd92a294e1430c63f2b
Sparsh Gupta <66106648+SparshOM@users.noreply.github.com>
2023-04-29 16:06:00 +0530

    Python

---
+++ python.py
@@ -0,0 +1,7 @@
+import math
+ 
+#l = [0.1]*10
+#print(math.fsum(l))
+
+num = 15.588
+print(math.floor(num),math.ceil(num),math.modf(num))
---
+++ set.py
@@ -0,0 +1,30 @@
+#s = {10,20,30,40,10,20,30,40}
+#s.add(50)
+#print(s)
+
+#s1 = {40,50,60,70,80,90}
+#s2= s.union(s1)
+#print(s2)
+#s3= s2.intersection(s)
+#print(s3)
+
+#s4= s1.difference(s)
+#print(s4)
+
+#s5= s1.symmetric_difference(s)
+#print(s5)
+
+#s.update(s1)
+#print(s)
+
+#s1.intersection_update(s)
+#print(s1)
+
+#s1.difference_update(s)
+#print(s1)
+
+s1 = {100,200,300,400,500}
+#s2 = {300,400,500}
+#print(s1.issuperset(s2))
+
+print(min(s1),max(s1))
