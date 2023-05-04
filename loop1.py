595d5c4b106fa292eccf2208b49e650ab57c1da8
Sparsh Gupta <66106648+SparshOM@users.noreply.github.com>
2023-05-04 17:09:08 +0530

    Python

--- loop1.py
+++ loop1.py
@@ -1,6 +1,6 @@
 #l1 = input("Enter Num start")
 #l2 = input("Enter Num start")
 sum =0
-for value in range(l1 = input("Enter Num start"),l2 = input("Enter Num start")):
+for value in range(l1 = input("Enter Number start"),l2 = input("Enter Number start")):
     sum = sum + value
 print(sum)
