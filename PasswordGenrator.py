fc8fd8f074944e77d0f12184a010d281c0e74e41
Sparsh Gupta <66106648+SparshOM@users.noreply.github.com>
2023-05-10 23:44:09 +0530

    Update PasswordGenrator.py

--- PasswordGenrator.py
+++ PasswordGenrator.py
@@ -1,12 +1,15 @@
 import random
 
-def Genrate_Password():
+def Genrate_Password(length=8):
     l= ['!','@','#','$','%']
     upper = chr(random.randint(65,90))
     lower = chr(random.randint(97,122))
     digits= random.randint(10000,99999)
     special = random.choice(l)
     password = upper+lower+str(digits)+special
-    return password
+    len = random.sample(password,length)
+    password = ("").join(len)
 
-print(Genrate_Password())
+    return password
+#length = int(input("Enter"))
+print(Genrate_Password())  
